af28a66dd1438020baef900762d068c8835e7acb
deepesh0203 <135854527+deepesh0203@users.noreply.github.com>
2023-08-20 14:16:24 +0530

    Update water_jug_problem.py

--- water_jug_problem.py
+++ water_jug_problem.py
@@ -10,16 +10,20 @@ def fill_jug():
              #Fill Jug1
              if (max[0],y) not in visited:
                     stack.append((max[0],y) )
-             #
+             #Fill Jug2
              if (x,max[1]) not in visited:
                  stack.append((x,max[1]) )
+             #Empty Jug1
              if (0,y) not in visited:
                  stack.append((0,y)) 
+             #Empty Jug2
              if (x,0) not in visited:
                  stack.append((x,0)) 
+             #Pour water from Jug1 to Jug2
              amount=min(x,max[1]-y)
              if amount>0:
                  stack.append((x-amount,y+amount))
+             #Pour water from Jug2 to Jug1
              amount=min(y,max[0]-x)
              if amount>0:
                  stack.append((x+amount,y-amount))
